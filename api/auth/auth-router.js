96211e839fbce263550e500407926babe0b02cea
Bennett Kautz <bennettkautz@gmail.com>
2022-07-29 14:22:30 -0400

    fixing next in authROuter

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -47,7 +47,7 @@ router.post('/register', async (req, res) => {
   */
 });
 
-router.post('/login', (req, res) => {
+router.post('/login', (req, res, next) => {
   if (!req.body.username || !req.body.password) {
     res.status(401).json({ message: "username and password required" })
     return
@@ -64,29 +64,7 @@ router.post('/login', (req, res) => {
         next({ status: 401, message: 'invalid credentials' })
       }
     })
-  /*
-    IMPLEMENT
-    You are welcome to build additional middlewares to help with the endpoint's functionality.
-
-    1- In order to log into an existing account the client must provide `username` and `password`:
-      {
-        "username": "Captain Marvel",
-        "password": "foobar"
-      }
-
-    2- On SUCCESSFUL login,
-      the response body should have `message` and `token`:
-      {
-        "message": "welcome, Captain Marvel",
-        "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8"
-      }
-
-    3- On FAILED login due to `username` or `password` missing from the request body,
-      the response body should include a string exactly as follows: "username and password required".
-
-    4- On FAILED login due to `username` not existing in the db, or `password` being incorrect,
-      the response body should include a string exactly as follows: "invalid credentials".
-  */
+  
 });
 
 module.exports = router;
--- api/user/user-router.js
+++ api/user/user-router.js
@@ -1,7 +1,12 @@
 const router = require('express').Router()
 
 router.get('/', (req, res, next) => {
-    res.json('users')
+    res.json(
+        {
+            username: "bennett",
+            paswword:"kautz123"
+        }
+    )
 })
 
 module.exports = router
